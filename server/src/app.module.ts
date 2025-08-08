1a2ec9bf611bb2c0f446adc3a1d61567ceef1040
Diego loeches <diego.loeches@nextdigital.es>
2025-08-08 13:47:07 +0200

    health check

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthModule } from './health/health.module';
+import { HealthController } from './health/health.controller';
+import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [HealthModule, TerminusModule],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ server/src/health/health.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheckService, HealthCheck } from '@nestjs/terminus';
+import { ApiTags, ApiOperation, ApiOkResponse } from '@nestjs/swagger';
+
+@ApiTags('Health')
+@Controller('health')
+export class HealthController {
+  constructor(private readonly health: HealthCheckService) {}
+
+  @Get()
+  @HealthCheck() // marks this route as a health check endpoint
+  @ApiOperation({ summary: 'Simple health check' })
+  @ApiOkResponse({ description: 'OK – Service is healthy' })
+  check() {
+    // no indicators passed: always returns status "ok"
+    return this.health.check([]);
+  }
+}
